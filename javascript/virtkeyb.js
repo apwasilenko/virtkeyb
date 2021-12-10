28ecb7cd309c5a73a512dea62bf77a7f6903036e
APWASILENKO <apwasilenko@gmail.com>
2021-12-10 23:06:44 +0700

    fixed key handling error

--- javascript/virtkeyb.js
+++ javascript/virtkeyb.js
@@ -1,26 +1,27 @@
-let myinput = document.getElementById('myintut');
+let input = document.getElementById('myintut');
 
 document.onclick = function(e){
-  if (typeof(e.target.attributes.name.value) != "undefined" && typeof(e.target.attributes.data.value) != "undefined") {
-    console.log("name.value - " + e.target.attributes.name.value);
-    console.log("data.value - " + e.target.attributes.data.value);
-    if (e.target.attributes.name.value == "bt") {
-      switch (e.target.attributes.data.value){
-          case "7":
-            console.log("Нажата клавиша - " + e.target.attributes.data.value);
-            break;
-
+    if (e.target.attributes.data != undefined){
+      if (e.target.attributes.data.value != undefined){
+        switch (e.target.attributes.data.value){
+          case "0":
+          case "1":
+          case "1":
+          case "2":
+          case "3":
+          case "4":
+          case "5":
+          case "6":
+          case "7":          
           case "8":
-            console.log("Нажата клавиша - " + e.target.attributes.data.value);
-            break;
-            
           case "9":
             console.log("Нажата клавиша - " + e.target.attributes.data.value);
+            input.innerHTML = input.innerHTML + e.target.attributes.data.value;
             break;
 
-          default:
-            console.log("Нет таких значений" );
+            default:
+              console.log("Нет таких значений" );
+        }
       }
     }
   }
-}
