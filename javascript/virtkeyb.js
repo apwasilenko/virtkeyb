8ed06fee9764bcc86baaed3df42de1a959563868
APWasilenko <APWasilenko@gmail.com>
2021-12-11 10:39:29 +0700

    added number input line

--- javascript/virtkeyb.js
+++ javascript/virtkeyb.js
@@ -1,27 +1,25 @@
-let input = document.getElementById('myintut');
+window.onload = function () {
+  myvirtkeyb = document.getElementById("virtkeyb");
+  document.onclick = function(e){
+    if (e.target.attributes.data != undefined && e.target.attributes.data.value != undefined){
+      switch (e.target.attributes.data.value){
+        case "0":
+        case "1":
+        case "1":
+        case "2":
+        case "3":
+        case "4":
+        case "5":
+        case "6":
+        case "7":          
+        case "8":
+        case "9":
+          myvirtkeyb.rows[0].cells[0].innerHTML = myvirtkeyb.rows[0].cells[0].innerHTML + e.target.attributes.data.value;
+          break;
 
-document.onclick = function(e){
-    if (e.target.attributes.data != undefined){
-      if (e.target.attributes.data.value != undefined){
-        switch (e.target.attributes.data.value){
-          case "0":
-          case "1":
-          case "1":
-          case "2":
-          case "3":
-          case "4":
-          case "5":
-          case "6":
-          case "7":          
-          case "8":
-          case "9":
-            console.log("Нажата клавиша - " + e.target.attributes.data.value);
-            input.innerHTML = input.innerHTML + e.target.attributes.data.value;
-            break;
-
-            default:
-              console.log("Нет таких значений" );
-        }
+          default:
+            console.log("Нет таких значений" );
       }
     }
   }
+}
